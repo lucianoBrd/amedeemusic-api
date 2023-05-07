d55207d330fe77dc8165a5fe752c68cba16a86e2
lucianoBrd <lucien.burdet@gmail.com>
2023-05-07 20:02:14 +0200

    manage testimonial

--- src/Controller/Admin/TestimonialCrudController.php
+++ src/Controller/Admin/TestimonialCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Testimonial;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class TestimonialCrudController extends AbstractCrudController
@@ -12,14 +15,11 @@ class TestimonialCrudController extends AbstractCrudController
         return Testimonial::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name')->setColumns(6);
+        yield TextField::new('designation')->setColumns(6);
+        yield AssociationField::new('local')->setColumns(12);
+        yield TextareaField::new('citation')->setColumns(12);
     }
-    */
 }
