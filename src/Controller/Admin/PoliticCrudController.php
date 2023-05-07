a75aa91822f9a527ba9f89107bc737e70eda5920
lucianoBrd <lucien.burdet@gmail.com>
2023-05-07 19:14:21 +0200

    manage politic

--- src/Controller/Admin/PoliticCrudController.php
+++ src/Controller/Admin/PoliticCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Politic;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PoliticCrudController extends AbstractCrudController
@@ -12,14 +15,14 @@ class PoliticCrudController extends AbstractCrudController
         return Politic::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('title');
+        yield TextEditorField::new('document')->setTrixEditorConfig([
+            'blockAttributes' => [
+                'default' => ['tagName' => 'p'],
+            ],
+        ]);
+        yield AssociationField::new('local');
     }
-    */
 }
