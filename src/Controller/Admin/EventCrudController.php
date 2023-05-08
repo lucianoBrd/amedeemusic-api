ab62c75e4c23e1324e661bf4490bdca64315fbbe
lucianoBrd <lucien.burdet@gmail.com>
2023-05-08 10:10:41 +0200

    manage event

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Event;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class EventCrudController extends AbstractCrudController
@@ -12,14 +16,12 @@ class EventCrudController extends AbstractCrudController
         return Event::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name')->setColumns(6);
+        yield TextField::new('place')->setColumns(6);
+        yield AssociationField::new('local')->setColumns(6);
+        yield DateTimeField::new('date')->setColumns(6);
+        yield UrlField::new('link')->setColumns(12);
     }
-    */
 }
